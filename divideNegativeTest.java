79b1179fdfd211b4294de96d841574ca6263c377
Genius Machado <44523632+GeniusMachado@users.noreply.github.com>
2022-03-26 22:36:53 -0400

    Update divideNegativeTest.java

--- divideNegativeTest.java
+++ divideNegativeTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 
-public class DivideNegative {
+public class divideNegativeTest {
 
 	@Test
 	public void test() {
